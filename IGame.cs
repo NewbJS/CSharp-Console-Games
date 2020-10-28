4a53202c895e0cb0745fc0d2bdf0837d5bdf908f
Colin Lewis <solartruth101@gmail.com>
2020-10-28 19:41:38 -0400

    Game interface because why not

--- IGame.cs
+++ IGame.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace GameInterface
 {
     interface IGame
     {
+
+        // public List<int> whatWrong;
         public void Game();
     }
 }
